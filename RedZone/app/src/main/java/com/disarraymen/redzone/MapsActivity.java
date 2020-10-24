74728a159f6f1d3a6b50f2814f17d04279024e9e
ghostwolf1995 <coppetta.andrea@gmail.com>
2020-10-24 18:01:55 +0200

    Removed notifications
    due to compatibility problems with API<26

--- RedZone/app/src/main/java/com/disarraymen/redzone/MapsActivity.java
+++ RedZone/app/src/main/java/com/disarraymen/redzone/MapsActivity.java
@@ -1,30 +1,22 @@
 package com.disarraymen.redzone;
 
-import androidx.annotation.ContentView;
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -38,96 +30,87 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private static final int REQUEST_LOCATION = 99;
-    private NotificationUtils mNotificationUtils;
+    private GoogleMap mMap;
+    private double currentlatitude;
+    private double currentlongitude;
+    private LocationManager locationManager;
 
-    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_maps);
 
-        mNotificationUtils = new NotificationUtils(this);
-
-        // Check permissions
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
-            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, REQUEST_LOCATION);
-
-        } else {
-            loadMap();
+        if (!isPermisionAccess()) {
+            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 99);
+            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 99);
         }
-    }
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    public Location positionGlobal = new Location("myGpsProvider");
-
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-        LocationManager mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        String bestProvider = mLocationManager.getBestProvider(new Criteria(), true);
-
-        positionGlobal = mLocationManager.getLastKnownLocation(bestProvider);
-        mLocationManager.requestLocationUpdates(bestProvider, 1000, 1, locationListenerGPS);
-
-        LatLng location = new LatLng(positionGlobal.getLatitude(), positionGlobal.getLongitude());
-        googleMap.addMarker(new MarkerOptions().position(location).title("Tu sei qui"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 12.0f));
-
-    }
-
-    private void loadMap() {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        assert mapFragment != null;
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_LOCATION) {// If request is cancelled, the result arrays are empty.
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // permission was granted! Do the location-related task.
-                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    // Request location:
-                    loadMap();
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        getCurrentLocation();
+        Toast.makeText(this, "latitude:" + currentlatitude + " longitude:" + currentlongitude, Toast.LENGTH_SHORT).show();
+
+        LatLng locationMarker = new LatLng(currentlatitude, currentlongitude);
+        mMap.addMarker(new MarkerOptions()
+                .position(locationMarker)
+                .title("Tu sei qui"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(locationMarker));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(locationMarker, 12.0f));
+
+    }
+
+
+
+
+    @SuppressLint("MissingPermission")
+    public void getCurrentLocation() {
+        if (isPermisionAccess()) {
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            if (locationManager != null) {
+                @SuppressLint("MissingPermission") Location gpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                @SuppressLint("MissingPermission") Location netLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                if (gpsLocation != null) {
+                    currentlatitude = gpsLocation.getLatitude();
+                    currentlongitude = gpsLocation.getLongitude();
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER , 1000, 0, locationListenerGPS);
+                } else if (netLocation != null) {
+                    currentlatitude = netLocation.getLatitude();
+                    currentlongitude = netLocation.getLongitude();
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER , 1000, 0, locationListenerGPS);
+
                 }
-            } else {
-                // permission denied! Disable the functionality that depends on this permission.
             }
         }
+
     }
+
+    private boolean isPermisionAccess() {
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED);
+    }
+
 
     LocationListener locationListenerGPS = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
             double latitude = location.getLatitude();
             double longitude = location.getLongitude();
-            positionGlobal.setLatitude(latitude);
-            positionGlobal.setLongitude(longitude);
             String msg = "Latitude: " + latitude + ", Longitude: " + longitude;
             // Write a message to the database
             FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference myRef = database.getReference("myLocation");
+            DatabaseReference myRef = database.getReference("usersLocations");
             // Read from the database
             myRef.addValueEventListener(new ValueEventListener() {
-                @RequiresApi(api = Build.VERSION_CODES.O)
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                     // This method is called once with the initial value and again
@@ -135,7 +118,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     String value = dataSnapshot.getValue(String.class);
                     //Log.d(TAG, "Value is: " + value);
                     Toast.makeText(getBaseContext(), value, Toast.LENGTH_LONG).show();
-                    sendNotification("Linea1", "Linea2 ->     " + value);
                 }
 
                 @Override
@@ -149,10 +131,4 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             //Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
         }
     };
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public void sendNotification(String line1, String line2) {
-        Notification.Builder nb = mNotificationUtils.getAndroidChannelNotification(line1, line2);
-        mNotificationUtils.getManager().notify(101, nb.build());
-    }
 }
